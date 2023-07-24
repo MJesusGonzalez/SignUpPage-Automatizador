4d968945b61e5a781d9588e469ec65a46db20686
M. Jesus Gonzalez <marcelogonzalez1418@hotmail.com>
2023-07-24 18:41:18 -0500

    webpack complete

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-// src/index.js
 import { initializeApp } from "firebase/app";
-import { getFirestore, doc, getDoc } from "firebase/firestore/lite";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+//createUserWithEmailAndPassword
 
-const firebaseApp = initializeApp({
-  /* config */
-});
-const db = getFirestore(firebaseApp);
+const firebaseConfig = {
+  apiKey: "AIzaSyC17J5Pna48y8qm_0IvRXtH8Hwmr5SFJHc",
+  authDomain: "pelisjesus-db427.firebaseapp.com",
+  projectId: "pelisjesus-db427",
+  storageBucket: "pelisjesus-db427.appspot.com",
+  messagingSenderId: "574701512207",
+  appId: "1:574701512207:web:9278b92827587fa51a257a",
+};
+const email = "marcelo@epn.edu.ec";
+const password = "123456";
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log("inicializado");
 
-async function loadCity(name) {
-  const cityDoc = doc(db, `cities/${name}`);
-  const snapshot = await getDoc(cityDoc);
-  return {
-    id: snapshot.id,
-    ...snapshot.data(),
-  };
-}
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    console.log("Usuario creado");
+    console.log(user.email);
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode, errorMessage);
+    // ..
+  });
