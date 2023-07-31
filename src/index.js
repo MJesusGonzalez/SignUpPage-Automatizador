67364da6ada87e70db2e99b349f663a353a26665
M. Jesus Gonzalez <marcelogonzalez1418@hotmail.com>
2023-07-31 12:47:57 -0500

    error alert

--- src/index.js
+++ src/index.js
@@ -27,6 +27,7 @@ createUserWithEmailAndPassword(auth, email, password)
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
+    alert(errorMessage);
     console.log(errorCode, errorMessage);
     // ..
   });
